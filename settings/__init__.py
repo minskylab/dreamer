246ff4436d604fca6c678a92ce762e026950eb6e
fsocualaya <fernando.socualaya@utec.edu.pe>
2020-09-04 22:13:54 -0500

    fix (persistence): Add password args as environment variables

--- settings/__init__.py
+++ settings/__init__.py
@@ -8,5 +8,6 @@ PORT: str = os.environ.get("PORT", "5000")
 
 ARANGO_URL: str = os.environ.get("ARANGO_URL", "http://127.0.0.1:8529")
 DATABASE_NAME: str = os.environ.get("DATABASE_NAME", "dreams")
+PASSWORD: str = os.environ.get("PASSWORD", 'root')
 
 COLLECTION_NAME: str = "dreams_v1"
