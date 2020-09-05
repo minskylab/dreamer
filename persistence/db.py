13232127193b9a07cf8856e4c77e449699b057c8
fsocualaya <fernando.socualaya@utec.edu.pe>
2020-09-04 21:57:35 -0500

    fix (persistence): Add password for db persistence

--- persistence/db.py
+++ persistence/db.py
@@ -8,11 +8,11 @@ logger.info(
     f"connecting to {ARANGO_URL}, db: {DATABASE_NAME}, coll: {COLLECTION_NAME}")
 client = arango.ArangoClient(hosts=ARANGO_URL)
 
-_sys_db: StandardDatabase = client.db()
+_sys_db: StandardDatabase = client.db(password='root')
 if not _sys_db.has_database(DATABASE_NAME):
     _sys_db.create_database(DATABASE_NAME)
 
-db = client.db(DATABASE_NAME)
+db = client.db(DATABASE_NAME, password='root')
 
 if db.has_collection(COLLECTION_NAME):
     dreams_collection: StandardCollection = db.collection(COLLECTION_NAME)
