62ecaec61a97101235c2f3a20ed92085a8d9e698
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-09-06 01:43:07 -0500

    feat: dependecies updated and new resolvers structure

--- service/resolvers.py
+++ service/resolvers.py
@@ -1,29 +1,26 @@
-from datetime import datetime
-from persistence.dreams import save_new_dream
-from entities import Dream, DreamDraft, Dreamer, DreamerDraft
-from persistence.db import dreams_collection
-from dataclasses import asdict
+from graphql_service import schema
+from persistence.dreams import save_dream
+from entities import DreamDraft
 
 # Using Starlette
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from starlette.graphql import GraphQLApp
 
-def save_dream(dream: DreamDraft) -> str:
-    dream.dreamer = DreamerDraft(**dream.dreamer)
-    print(f"saving dream of {dream.dreamer.name}.")
-    new_dream = save_new_dream(dream)
-    return new_dream.id
 
-app = Starlette(debug=True)
+routes = [
+    Route('/graphql', GraphQLApp(schema=schema))
+]
+
+app = Starlette(debug=True, routes=routes)
+
 
 @app.route('/register-dream', methods=["POST"])
 async def register_dream(request):
     data = await request.json()
-    print(data['dreamer'])
     obj: DreamDraft = DreamDraft(data['dreamer'], data['dream'], data['date'])
     dream_id = save_dream(obj)
     res = JSONResponse({"id": dream_id})
     return res
-
