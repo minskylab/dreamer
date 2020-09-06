a55d1185b7106e449c4a54a35a2cb8ba36d2412e
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-09-05 23:43:17 -0500

    fix: changes in env variables

--- persistence/db.py
+++ persistence/db.py
@@ -1,18 +1,18 @@
 import arango
 from arango.database import StandardCollection, StandardDatabase
 from loguru import logger
-from settings import ARANGO_URL, DATABASE_NAME, COLLECTION_NAME, PASSWORD
+from settings import ARANGO_ENDPOINT, DATABASE_NAME, COLLECTION_NAME, ARANGO_PASSWORD
 
 
 logger.info(
-    f"connecting to {ARANGO_URL}, db: {DATABASE_NAME}, coll: {COLLECTION_NAME}")
-client = arango.ArangoClient(hosts=ARANGO_URL)
+    f"connecting to {ARANGO_ENDPOINT}, db: {DATABASE_NAME}, coll: {COLLECTION_NAME}")
+client = arango.ArangoClient(hosts=ARANGO_ENDPOINT)
 
-_sys_db: StandardDatabase = client.db(password=PASSWORD)
+_sys_db: StandardDatabase = client.db(password=ARANGO_PASSWORD)
 if not _sys_db.has_database(DATABASE_NAME):
     _sys_db.create_database(DATABASE_NAME)
 
-db = client.db(DATABASE_NAME, password=PASSWORD)
+db = client.db(DATABASE_NAME, password=ARANGO_PASSWORD)
 
 if db.has_collection(COLLECTION_NAME):
     dreams_collection: StandardCollection = db.collection(COLLECTION_NAME)
--- settings/__init__.py
+++ settings/__init__.py
@@ -6,8 +6,10 @@ dotenv.load_dotenv()
 HOST: str = os.environ.get("HOST", "0.0.0.0")
 PORT: int = os.environ.get("PORT", 5000)
 
-ARANGO_URL: str = os.environ.get("ARANGO_URL", "http://127.0.0.1:8529")
+ARANGO_ENDPOINT: str = os.environ.get(
+    "ARANGO_ENDPOINT", "http://127.0.0.1:8529")
+ARANGO_PASSWORD: str = os.environ.get("ARANGO_PASSWORD", 'root')
+
 DATABASE_NAME: str = os.environ.get("DATABASE_NAME", "dreams")
-PASSWORD: str = os.environ.get("PASSWORD", 'root')
 
 COLLECTION_NAME: str = "dreams_v1"
